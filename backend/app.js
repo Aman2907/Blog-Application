e7f1563523d20f41e3c2a53db33a48e0709cb2d3
Aman Sharma <74008888+Aman2907@users.noreply.github.com>
2024-12-08 20:46:42 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,13 @@ import blogRouter from "./routes/bblog-routes.js";
 import cors from 'cors';
 
   
-  
 const app = express()
 app.use(cors()) 
 app.use(express.json())
 app.use("/api/user", router); 
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://amandeo2907:k7RiOAggcDQqyysY@cluster0.idktepw.mongodb.net/blogging')
+mongoose.connect('MONGOENV)
 .then(() => app.listen(5000))
 .then(() => console.log("Connected to database and listening to 5000"))
 .catch((err) => console.log(err));
